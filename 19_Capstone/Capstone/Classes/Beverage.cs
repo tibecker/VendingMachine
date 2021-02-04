c82be1d36ff9db29b2423d643b7633cd79a4b312
Tim Becker <rekcebt@gmail.com>
2021-02-04 14:45:08 -0500

    first commit

---
+++ 19_Capstone/Capstone/Classes/Beverage.cs
@@ -0,0 +1,23 @@
+﻿using Capstone.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.CLI
+{
+    public class Beverage : Food
+    {
+        override public string Message
+        {
+            get
+            {
+                return "Glug, Glug, Yum!";
+            }
+        }
+
+        public Beverage(string product, int quantity, double price, string slot): base(product, quantity, price, slot)
+        {
+
+        }
+    }
+}
---
+++ 19_Capstone/Capstone/Classes/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    abstract public class Food
+    {
+        public string Product { get; }
+        public int Quantity { get; set; }
+        public double Price { get; }
+        public string Slot { get; }
+        virtual public string Message { get; }
+
+        public Food(string product, int quantity, double price, string slot)
+        {
+            this.Product = product;
+            this.Quantity = quantity;
+            this.Price = price;
+            this.Slot = slot;
+        }
+
+    }
+}
