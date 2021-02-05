a856a9d14f1b31f91975a673d1298167dd8f4797
Clayton Cross <crosscribbs@gmail.com>
2021-02-05 08:56:33 -0500

    Commit 4

--- 19_Capstone/Capstone/Classes/VendingMachine.cs
+++ 19_Capstone/Capstone/Classes/VendingMachine.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Capstone.Classes;
 
 namespace Capstone.CLI
 {
-    class VendingMachine
+     public class VendingMachine
     {
+
+        public decimal Balance { get; set; } = 0;
+
+
+        public decimal FeedMoney(int input)
+        {
+            Balance += input;
+
+            return Balance;
+        }
+
+        public List<string> SelectProduct()
+        {
+            //check if products are available
+            foreach (KeyValuePair<string, Food> kvp in product )
+
+            // if they have enough money for product
+        }
+
+        
+
     }
 }
